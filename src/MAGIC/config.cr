6bff4e3ed45c4bae01da8ab239f1dc305fd31eb2
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-06-25 14:06:41 +0200

    Add files via upload

--- src/MAGIC/config.cr
+++ src/MAGIC/config.cr
@@ -1,30 +1,30 @@
-﻿<appSettings>
+<appSettings>
   <!-- ____________________________________ Server SETTINGS ____________________________________ -->
   <add key="ClientVersion" value="10.134" />
   <!-- Client version that will be able to access the server-->
-  <add key="UpdateUrl" value="http://antzps.ddns.net/downloads/" />
+  <add key="UpdateUrl" value="http://antzps.ddns.net" />
   <!-- Location to redirect outdated player-->
   <add key="MaintenanceTime" value="20" />
   <!-- Maintenance Time in Minutes-->
-  <add key="PatchUrl" value="http://antzps.ddns.net/Patchs/" />
+  <add key="PatchUrl" value="http://192.168.43.154:8088/Patchs/" />
   <!-- Location of your patching server-->
-  <add key="EventsUrl" value="http://antzps.ddns.net/events/" />
+  <add key="EventsUrl" value="http://192.168.43.154:8088/events/" />
   <!-- Location of your events folder-->
-  
+  <add key="AdminMessage" value="haz not work" />
+  <!-- The AdminMessage will be shown in the Inbox of the Players-->
   <add key="RedisIPAddress" value="127.0.0.1" />
   <!-- Redis server IP address-->
   <add key="RedisPort" value="6379" />
   <!--  Redis server port-->
   <add key="RedisPassword" value="" />
   <!--  Redis server password-->
-
   <add key="MysqlIPAddress" value="127.0.0.1" />
   <!-- MySql IP Address -->
   <add key="MysqlPort" value="3306" />
   <!-- MySql Username -->
   <add key="MysqlUsername" value="root" />
   <!-- MySql Username -->
-  <add key="MysqlPassword" value="ascent" />
+  <add key="MysqlPassword" value="" />
   <!--  MySql server password-->
   <add key="MysqlDatabase" value="Clash" />
   <!-- Mysql Database -->
