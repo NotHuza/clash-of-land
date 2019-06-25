210eed6afdfa45de88515960b5a2273f06569344
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-06-25 13:58:32 +0200

    Add files via upload

--- src/MAGIC/Extensions/Constants.cs
+++ src/MAGIC/Extensions/Constants.cs
@@ -5,36 +5,33 @@ using System.Reflection;
 namespace ClashLand.Extensions
 {
     internal class Constants
-    {
+       {
         internal const int ID = 0;
         internal const int MaxCommand  = 0;
         internal const int Buffer = 4096;
-        internal const int PRE_ALLOC_SEA = 4096;
+        internal const int PRE_ALLOC_SEA = 128;
         internal const bool Local = false;
         internal const bool PacketCompression = true;
         internal const bool UseSentry = false;
         internal const bool RC4 = true;
-        internal const DBMS database = DBMS.Both;
+        internal const DBMS Database = DBMS.Mysql;
+        //internal const DBMS Database = DBMS.Both;
         public static string Version = Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
         internal static int MaintenanceDuration = 0;
         internal static Maintenance_Timer Maintenance = null;
         internal static readonly string PatchServer = Utils.ParseConfigString("PatchUrl");
-        internal static readonly DBMS Database = (DBMS)Utils.ParseConfigInt("DatabaseMode");
         internal static readonly string UpdateServer = Utils.ParseConfigString("UpdateUrl");
         internal static readonly string[] ClientVersion = Utils.ParseConfigString("ClientVersion").Split('.');
         internal static readonly string Events = Utils.ParseConfigString("EventsUrl");
+        internal static readonly string AdminMessage = Utils.ParseConfigString("AdminMessage");
 
         internal static string[] AuthorizedIP =
         {
-            "192.168.0.18",
+            "192.168.0.5",
             "192.168.0.144",
             "115.133.41.158"
         };
 
-        //internal static DBMS Database => database;
-
-        //public static bool Database { get; internal set; }
-        //internal static readonly DBMS Database = (DBMS)Utils.ParseConfigInt("DatabaseMode");
     }
 }
