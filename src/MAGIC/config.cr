4acebfcaf22d7ff5540a8414efe985060d9eb594
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-07-02 09:12:21 +0200

    Add files via upload

---
+++ src/MAGIC/config.cr
@@ -0,0 +1,32 @@
+<appSettings>
+  <!-- ____________________________________ Server SETTINGS ____________________________________ -->
+  <add key="ClientVersion" value="10.134" />
+  <!-- Client version that will be able to access the server-->
+  <add key="UpdateUrl" value="http://127.0.0.1" />
+  <!-- Location to redirect outdated player-->
+  <add key="MaintenanceTime" value="20" />
+  <!-- Maintenance Time in Minutes-->
+  <add key="PatchUrl" value="http:http://127.0.0.1/Patchs/" />
+  <!-- Location of your patching server-->
+  <add key="EventsUrl" value="http:http://127.0.0.1/events/" />
+  <!-- Location of your events folder-->
+  <add key="AdminMessage" value="haz not work" />
+  <!-- The AdminMessage will be shown in the Inbox of the Players-->
+  <add key="RedisIPAddress" value="127.0.0.1" />
+  <!-- Redis server IP address-->
+  <add key="RedisPort" value="6379" />
+  <!--  Redis server port-->
+  <add key="RedisPassword" value="" />
+  <!--  Redis server password-->
+  <add key="MysqlIPAddress" value="127.0.0.1" />
+  <!-- MySql IP Address -->
+  <add key="MysqlPort" value="3306" />
+  <!-- MySql Username -->
+  <add key="MysqlUsername" value="root" />
+  <!-- MySql Username -->
+  <add key="MysqlPassword" value="" />
+  <!--  MySql server password-->
+  <add key="MysqlDatabase" value="Clash" />
+  <!-- Mysql Database -->
+  <!-- _______________________________________________________________________________________ -->
+</appSettings>
