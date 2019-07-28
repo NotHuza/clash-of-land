43de19b39641406d5b0f8baf5e25a7814604bbbe
free jacow gwn <jakovmarkovanovic@gmail.com>
2019-07-28 17:43:29 +0200

    add loger.cs

---
+++ src/MAGIC/Core/loger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClashLand.Core
+{
+    class Loger
+    {
+        public static void Say(string message, bool write = false)
+        {
+            if (!write)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ResetColor();
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.ResetColor();
+                Console.Write(message);
+            }
+        }
+
+        public static void Say()
+        {
+            Console.WriteLine();
+        }
+    }
+}
