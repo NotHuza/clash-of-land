5a5a37f2113a486cd463f7d86737184347795b44
Antz <15341485+antzsmt@users.noreply.github.com>
2019-12-27 21:13:36 +0800

    Add files via upload

---
+++ src/MAGIC/Packets/Commands/Client/SpeedupTrainV2.cs
@@ -0,0 +1,25 @@
+﻿using ClashLand.Logic;
+using ClashLand.Extensions.Binary;
+
+namespace ClashLand.Packets.Commands.Client
+{
+
+
+    internal class SpeedupTrainV2 : Command
+    {
+        public SpeedupTrainV2(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.Debug();
+        }
+
+        internal override void Process()
+        {
+            base.Process();
+        }
+    }
+}
