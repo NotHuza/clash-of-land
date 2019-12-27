67923b40379c62757e200b38bfbb826ae4b0056e
Antz <15341485+antzsmt@users.noreply.github.com>
2019-12-27 21:09:01 +0800

    Add files via upload

---
+++ src/MAGIC/Packets/Commands/Client/CancelUnitProductionV2.cs
@@ -0,0 +1,25 @@
+﻿using ClashLand.Logic;
+using ClashLand.Extensions.Binary;
+
+namespace ClashLand.Packets.Commands.Client
+{
+    
+
+    internal class CancelUnitProductionV2 : Command
+    {
+        public CancelUnitProductionV2(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        internal override void Decode()
+        {
+            this.Debug();
+        }
+
+        internal override void Process()
+        {
+            base.Process();
+        }
+    }
+}
