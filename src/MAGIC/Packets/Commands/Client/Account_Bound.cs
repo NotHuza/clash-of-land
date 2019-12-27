eb43ac5219b8111ee2212bfb344780e8dd253a0d
Antz <15341485+antzsmt@users.noreply.github.com>
2019-12-27 21:06:09 +0800

    Add files via upload

---
+++ src/MAGIC/Packets/Commands/Client/Account_Bound.cs
@@ -0,0 +1,13 @@
+﻿using ClashLand.Extensions.Binary;
+using ClashLand.Logic;
+
+namespace ClashLand.Packets.Commands.Client
+{
+    // Packet 603
+    internal class Account_Bound : Command
+    {
+        public Account_Bound(Reader reader, Device client, int id) : base(reader, client, id)
+        {
+        }
+    }
+}
