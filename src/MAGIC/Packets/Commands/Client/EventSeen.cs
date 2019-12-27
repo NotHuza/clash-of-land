4cde6d0ff1759965fec75d4a3d0ee0c1c3f1d65b
Antz <15341485+antzsmt@users.noreply.github.com>
2019-12-27 21:10:43 +0800

    Add files via upload

---
+++ src/MAGIC/Packets/Commands/Client/EventSeen.cs
@@ -0,0 +1,22 @@
+﻿namespace ClashLand.Packets.Commands.Client
+{
+    using ClashLand.Logic;
+    using ClashLand.Extensions.Binary;
+
+    internal class EventSeen : Command
+    {
+        public EventSeen(Reader Reader, Device Device, int Identifier) : base(Reader, Device, Identifier)
+        {
+            
+        }
+
+        public int Tick { get; private set; }
+
+        internal override void Decode()
+        {
+            this.Reader.ReadInt32();
+
+            this.Tick = this.Reader.ReadInt32();
+        }
+    }
+}
