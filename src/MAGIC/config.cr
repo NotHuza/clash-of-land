cded4b80bc819425a16d5020fbd80f89bb9ece39
free jacow gwn <jakovmarkovanovic@gmail.com>
2020-01-18 18:22:18 +0100

    update for bot

--- src/MAGIC/config.cr
+++ src/MAGIC/config.cr
@@ -2,13 +2,13 @@
   <!-- ____________________________________ Server SETTINGS ____________________________________ -->
   <add key="ClientVersion" value="10.134" />
   <!-- Client version that will be able to access the server-->
-  <add key="UpdateUrl" value="your url" />
+  <add key="UpdateUrl" value="http://127.0.0.1/downloads/" />
   <!-- Location to redirect outdated player-->
   <add key="MaintenanceTime" value="20" />
   <!-- Client version that will be able to access the server-->
-  <add key="PatchUrl" value="your url" />
+  <add key="PatchUrl" value="http://127.0.0.1/Patchs/" />
   <!-- Location of your patching server-->
-  <add key="EventsUrl" value="your url" />
+  <add key="EventsUrl" value="http://127.0.0.1/events/" />
   <!-- Location of your events folder-->
   <add key="AdminMessage" value="Welcome to ClashLand" />
   <!-- The AdminMessage will be shown in the Inbox of the Players-->
@@ -18,7 +18,7 @@
   <!-- Redis server IP address-->
   <add key="RedisPort" value="6379" />
   <!--  Redis server port-->
-  <add key="RedisPassword" value="" />
+  <add key="RedisPassword" value="root" />
   <!--  Redis server password-->
   <add key="MysqlIPAddress" value="127.0.0.1" />
   <!-- MySql IP Address -->
@@ -26,23 +26,23 @@
   <!-- MySql Username -->
   <add key="MysqlUsername" value="root" />
   <!-- MySql Username -->
-  <add key="MysqlPassword" value="" />
+  <add key="MysqlPassword" value="root" />
   <!--  MySql server password-->
   <add key="MysqlDatabase" value="Clash" />
   <!-- Mysql Database -->
   <add key="Port" value="9339" />
-  <!-- server port defualt 9339-->
+  <!-- server port default 9339-->
   <add key="LogLevel" value="0"/>
   <!-- No Log = 0, Full Log = 1, Log only = 2 -->
   <add key="Verbosity" value="2"/>
-   <!-- No Log = 0, Full Log = 1, Log only = 2 -->
+  <!-- No Log = 0, Full Log = 1, Log only = 2 -->
   <add key="UseDiscord" value="false" />
-   <!-- Enables Discord Bot -->
+  <!-- Enables Discord Bot -->
   <add key="DiscordPrefix" value="?" />
   <!-- Discord Bot Prefix -->
   <add key="DiscordToken" value="NjYzNjk3NjAwMTk2NjQwNzY4.XhMTRQ.L4iFuYhzrPQxzBzHpxhTznHtJRs" />
-<!-- Discord Bot Token -->
-  <add key="DiscordServerName" value="Huza Clash" />
-   <!-- Discord Bot Server Name -->
+  <!-- Discord Bot Token -->
+  <add key="DiscordServerName" value="tetris" />
+  <!-- Discord Bot Server Name -->
   <!-- _______________________________________________________________________________________ -->
 </appSettings>
